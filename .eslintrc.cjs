1df6e61275bf359cb42015afe0fec590a06730bb
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-04-24 23:33:37 +0900

    config: 프로젝트 초기 설정

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+    'prettier',
+    'plugin:storybook/recommended',
+    'plugin:tailwindcss/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'react-refresh'],
+};
