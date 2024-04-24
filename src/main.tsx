c6674d03f10a57f4d8ed6277ba044a9f907e8c4d
김도영 <difbfl4750@gmail.com>
2024-04-25 00:17:32 +0900

    feat: footer 구현

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import App from './App.tsx';
-import './index.css';
+import Footer from './pages/footer.tsx';
+import './styles/index.css';
 
 const queryClient = new QueryClient();
 
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <Footer />
     </QueryClientProvider>
   </React.StrictMode>,
 );
