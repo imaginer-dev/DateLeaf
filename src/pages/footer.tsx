8f98ffd329c15bb1f0eabab926987a32d7ad3bad
김도영 <difbfl4750@gmail.com>
2024-04-25 23:02:26 +0900

    fix: merge on dev

--- src/pages/footer.tsx
+++ src/pages/footer.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Footer: React.FC = () => {
     return (
-        <footer className="footer footer-center bg-base-200 text-base-content rounded p-10">
+        <footer className="footer footer-center  rounded bg-base-200 p-10 text-base-content">
             <nav className="grid grid-flow-col gap-4">
-                <a className="link link-hover">About us</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Jobs</a>
-                <a className="link link-hover">Press kit</a>
+                <a className="link-hover link">About us</a>
+                <a className="link-hover link">Contact</a>
+                <a className="link-hover link">Jobs</a>
+                <a className="link-hover link">Press kit</a>
             </nav> 
             <nav>
                 <div className="grid grid-flow-col gap-4">
