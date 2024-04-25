4d1b40430c5fc3fd403d5667d758fbda99872968
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-04-25 19:42:57 +0900

    config: 리액트 라우터 돔 추가 및 초기 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import './styles/index.css';
 
 import App from './App.tsx';
-import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
