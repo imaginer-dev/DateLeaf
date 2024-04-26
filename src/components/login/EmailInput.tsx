2c1046caa34ac99f5d70ef5e06438c699acd8fab
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-04-26 16:49:21 +0900

    feat: 이메일 인풋 생성

---
+++ src/components/login/EmailInput.tsx
@@ -0,0 +1,7 @@
+import InputForm from '../common/InputForm.tsx';
+
+const EmailInput = () => {
+  return <InputForm title={'Email'} placeholder={'이메일을 입력하세요'} hint={'Hint Text'} onChange={() => {}} />;
+};
+
+export default EmailInput;
