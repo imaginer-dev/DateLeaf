b72a2808bffa1872660099ecc0c06ab73a8ad28d
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-04-26 16:50:04 +0900

    feat: 로그인 페이지 생성 및 로그인 라우트 생성

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+import LoginPageLayout from '../layouts/LoginPageLayout.tsx';
+import VerticalLogo from '../components/common/VerticalLogo.tsx';
+
+const LoginPage = () => {
+  return (
+    <LoginPageLayout>
+      <VerticalLogo />
+      {/*<LoginForm></LoginForm>*/}
+    </LoginPageLayout>
+  );
+};
+
+export default LoginPage;
