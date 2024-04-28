2725261cbb6479eb311655ad185fe0c0c161b9d2
hyeonjeong <hjung9248@gmail.com>
2024-04-28 22:58:02 +0900

    refactor: mv footer from pages to components folder

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,8 +1,7 @@
 import EditPwPage from './EditPwPage';
-import Footer from './Footer';
 import JoinPage from './JoinPage';
 import Loading from './Loading';
 import LoginPage from './LoginPage';
 import { NotFound } from './Notfound';
 
-export { EditPwPage, Footer, JoinPage, Loading, LoginPage, NotFound };
+export { EditPwPage, JoinPage, Loading, LoginPage, NotFound };
