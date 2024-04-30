5e1b14f8806846ccc6d81c1e0edfb8cf13b4c435
Lee Ye Seo <rosefor2ne@gmail.com>
2024-04-30 19:53:51 +0900

    feat: 이용동의 및 개인정보 동의 페이지 추가

---
+++ src/pages/Policy/PersonalInfoPage.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import PolicyPersonalInfo from '../../components/Policy/PolicyPersonalInfo';
+
+const PersonalInfoPage: FC = () => {
+  return (
+    <div>
+      <PolicyPersonalInfo />
+    </div>
+  );
+};
+
+export default PersonalInfoPage;
---
+++ src/pages/Policy/UseConditionPage.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import PolicyUseCondition from '../../components/Policy/PolicyUseCondition';
+
+const UseConditionPage: FC = () => {
+  return (
+    <div>
+      <PolicyUseCondition />
+    </div>
+  );
+};
+
+export default UseConditionPage;
