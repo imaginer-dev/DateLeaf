3253396829082616fc23a905354dd720568fdaff
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-05-01 11:04:04 +0900

    feat: 로그인 로직 구현

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,11 +1,45 @@
-import { FC, ReactNode } from 'react';
+import { FC, FormEventHandler, ReactNode } from 'react';
+import { useLoginState } from '../../stores/loginStore.ts';
+import { isValidEmail, isValidPassword } from '../../utils/authUtils.ts';
+import useSignIn from '../../react-queries/useSignIn.ts';
 
 interface Props {
   children: ReactNode;
 }
 
 const LoginForm: FC<Props> = ({ children }) => {
-  return <form className={'flex w-full flex-col'}>{children}</form>;
+  const { email, password } = useLoginState();
+  const { mutate } = useSignIn();
+  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    if (!isValidEmail(email) || !isValidPassword(password)) {
+      // TODO: 다이얼로그 보여 주기
+    }
+
+    mutate(
+      {
+        email,
+        password,
+      },
+      {
+        onError: (error) => {
+          console.error(error.message);
+        },
+        onSettled: (data) => {
+          console.log(data);
+        },
+        onSuccess: (data) => {
+          console.log(data);
+        },
+      },
+    );
+  };
+
+  return (
+    <form onSubmit={onSubmit} className={'flex w-full flex-col'}>
+      {children}
+    </form>
+  );
 };
 
 export default LoginForm;
