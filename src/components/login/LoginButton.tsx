3c6c9d48a5ab23fea4699bcb820ed366660774cc
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-05-01 11:16:45 +0900

    fix: 로그인 버튼 button 타입으로 수정 및 로그인 로직 버튼으로 이동

--- src/components/login/LoginButton.tsx
+++ src/components/login/LoginButton.tsx
@@ -1,15 +1,39 @@
 import { useLoginState } from '../../stores/loginStore.ts';
+import useSignIn from '../../react-queries/useSignIn.ts';
+import { isValidEmail, isValidPassword } from '../../utils/authUtils.ts';
 
 const LoginButton = () => {
   const { email, password } = useLoginState();
+  const { mutate } = useSignIn();
 
   const onClick = () => {
-    console.log('email: ', email);
-    console.log('password: ', password);
+    if (!isValidEmail(email) || !isValidPassword(password)) {
+      // TODO: 다이얼로그 보여 주기
+      console.log('not valid form');
+      return;
+    }
+
+    mutate(
+      {
+        email,
+        password,
+      },
+      {
+        onError: (error) => {
+          console.error(error.message);
+        },
+        onSettled: (data) => {
+          console.log(data);
+        },
+        onSuccess: (data) => {
+          console.log(data);
+        },
+      },
+    );
   };
 
   return (
-    <button type={'submit'} onClick={onClick} className="btn btn-outline btn-primary w-full">
+    <button type={'button'} onClick={onClick} className="btn btn-outline btn-primary w-full">
       SIGN IN
     </button>
   );
--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,45 +1,11 @@
-import { FC, FormEventHandler, ReactNode } from 'react';
-import { useLoginState } from '../../stores/loginStore.ts';
-import { isValidEmail, isValidPassword } from '../../utils/authUtils.ts';
-import useSignIn from '../../react-queries/useSignIn.ts';
+import { FC, ReactNode } from 'react';
 
 interface Props {
   children: ReactNode;
 }
 
 const LoginForm: FC<Props> = ({ children }) => {
-  const { email, password } = useLoginState();
-  const { mutate } = useSignIn();
-  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
-    e.preventDefault();
-    if (!isValidEmail(email) || !isValidPassword(password)) {
-      // TODO: 다이얼로그 보여 주기
-    }
-
-    mutate(
-      {
-        email,
-        password,
-      },
-      {
-        onError: (error) => {
-          console.error(error.message);
-        },
-        onSettled: (data) => {
-          console.log(data);
-        },
-        onSuccess: (data) => {
-          console.log(data);
-        },
-      },
-    );
-  };
-
-  return (
-    <form onSubmit={onSubmit} className={'flex w-full flex-col'}>
-      {children}
-    </form>
-  );
+  return <form className={'flex w-full flex-col'}>{children}</form>;
 };
 
 export default LoginForm;
