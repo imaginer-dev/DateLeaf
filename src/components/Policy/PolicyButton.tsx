a9644c9732df9986279d824b3773e69203f53e68
Lee Ye Seo <rosefor2ne@gmail.com>
2024-05-02 13:56:59 +0900

    design: policy button 위치 및 반응형 수정

--- src/components/Policy/PolicyButton.tsx
+++ src/components/Policy/PolicyButton.tsx
@@ -8,8 +8,8 @@ const PolicyButton = () => {
   };
 
   return (
-    <div className="absolute bottom-12 w-full max-w-5xl">
-      <button type={'submit'} onClick={onClick} className="btn btn-outline btn-primary display m-auto">
+    <div className="absolute bottom-12 w-10/12 md:w-11/12">
+      <button type={'submit'} onClick={onClick} className="btn btn-outline btn-primary m-auto block w-full max-w-md">
         동의하기
       </button>
     </div>
