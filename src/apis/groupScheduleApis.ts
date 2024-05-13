7b7e680a2d13d8fe8f0dac02927ca28c26ebdb36
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-05-13 23:37:09 +0900

    feat: 그룹 일정 등록 구현

--- src/apis/groupScheduleApis.ts
+++ src/apis/groupScheduleApis.ts
@@ -6,17 +6,47 @@ interface addGroupShedule {
   startDate: string;
   endDate: string;
   memo: string;
-  newMemberList: any[];
+  newMemberList: Member[];
 }
 
-export const addGroupSchedule = async ({ startDate, endDate, ...props }: addGroupShedule) => {
-  const { error } = await supabase
-    .from('group_schedules')
-    .insert({ ...props, start_date: startDate, end_date: endDate });
+export const addGroupScheduleFetch = async ({ startDate, endDate, newMemberList, ...props }: addGroupShedule) => {
+  // 그룹을 생성한다.
+  const { data: groupInfo, error: groupInfoError } = await supabase
+    .from('groups')
+    .insert({
+      name: props.title,
+      open: true,
+      start_date: startDate,
+      end_date: endDate,
+    })
+    .select()
+    .single();
 
-  if (error) {
-    throw error;
+  if (!groupInfo || groupInfoError) {
+    throw groupInfoError;
   }
+
+  // 그룹에 멤버를 추가한다.
+  const { error: groupMemberError } = await supabase.from('group_user_ralations').insert(
+    newMemberList.map((member) => ({
+      group_id: groupInfo.id,
+      user_id: member.id,
+    })),
+  );
+  if (groupMemberError) {
+    throw groupMemberError;
+  }
+  // 그룹 일정을 생성한다.
+  const { data: groupScheduleData, error: groupScheduleError } = await supabase
+    .from('group_schedules')
+    .insert({ ...props, start_date: startDate, end_date: endDate, group_id: groupInfo.id })
+    .select()
+    .single();
+
+  if (groupScheduleError) {
+    throw groupScheduleError;
+  }
+  return groupScheduleData;
 };
 
 interface UpdateGroupSchedule {
--- src/pages/AddGroupSchedulePage.tsx
+++ src/pages/AddGroupSchedulePage.tsx
@@ -1,27 +1,22 @@
-import { addGroupSchedule } from '@/apis/groupScheduleApis';
 import GroupForm from '@/components/Group/GroupForm';
 import AppBar from '@/components/common/AppBar';
+import Dialog from '@/components/common/Dialog';
+import { useAddGroupSchedule } from '@/hooks/useAddGroupSchedule';
 import { Member } from '@/types/Member';
 
 const AddGroupPage = () => {
+  const { addGroupSchedule, isPending, failDialogRef, successDialogRef } = useAddGroupSchedule();
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>, userList: Member[]) => {
     e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const addSchedule = {
-      title: formData.get('name') as string,
-      description: formData.get('description') as string,
-      startDate: formData.get('startDate') as string,
-      endDate: formData.get('endDate') as string,
-      memo: formData.get('memo') as string,
-      newMemberList: userList,
-    };
-    addGroupSchedule(addSchedule);
+    addGroupSchedule(e, userList);
   };
 
   return (
     <div className="flex min-h-dvh w-screen flex-col overflow-x-hidden px-6">
       <AppBar backButton title={'모임 일정 등록하기'} />
-      <GroupForm onSubmit={handleSubmit} />
+      <GroupForm onSubmit={handleSubmit} isLoading={isPending} />
+      <Dialog ref={successDialogRef} desc="모임 일정이 등록되었습니다." />
+      <Dialog ref={failDialogRef} desc="모임 일정 등록에 실패했습니다." />
     </div>
   );
 };
