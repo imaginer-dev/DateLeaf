0cef34917313df68458f4e9027efc6ea7e0f3545
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-05-13 11:55:09 +0900

    fix: dateUtils 수정으로 인한 테스트 코드 수정

--- src/utils/__test__/dateUtils.test.ts
+++ src/utils/__test__/dateUtils.test.ts
@@ -3,8 +3,7 @@ import { dateToYYMMDD } from '../dateUtils';
 
 describe('dateUtils', () => {
   it('should return a date string', () => {
-    const date = new Date('2021-09-01');
-    const result = dateToYYMMDD(date);
+    const result = dateToYYMMDD(new Date('2021-09-01').toString());
     expect(result).toBe('2021-09-01');
   });
 });
