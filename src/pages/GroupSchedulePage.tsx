598b3db3038bee294398402f5c79e20b50fc6bf7
hyeonjeong <hjung9248@gmail.com>
2024-05-15 00:30:53 +0900

    feat: 그룹 스케쥴 페이지에서 캘린더 param 수정

--- src/pages/GroupSchedulePage.tsx
+++ src/pages/GroupSchedulePage.tsx
@@ -5,6 +5,7 @@ import CreateEventButton from '@/components/MyCalendar/CreateEventButton';
 import { useParams } from 'react-router-dom';
 import { useGetOneGroupSchedule } from '@/react-queries/useGetOneGroupSchedule';
 import { Loading } from '.';
+import { DB_Events } from '@/utils';
 
 const GroupSchedulePage = () => {
   // TODO: 캘린더 수정해서 이벤트 리스트를 외부에서 받을 수 있도록 해야함.
@@ -20,6 +21,16 @@ const GroupSchedulePage = () => {
 
   console.log(data);
 
+  const db_events: DB_Events[] = [];
+  if (data) {
+    db_events.push({ ...data, start_date: data.start_date, end_date: data.end_date, title: data.title, id: data.id });
+  }
+
+  const onDelete = (id: number) => {
+    console.log('delete group id: ', id);
+    // TODO: delete group schedule with id
+  };
+
   return (
     <div className="lg:ml-80">
       {isLoading && <Loading size="lg" display="spinner" color="primary" />}
@@ -27,7 +38,7 @@ const GroupSchedulePage = () => {
       <main className="z-1 relative flex-grow">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="rounded bg-white p-6 px-4 sm:px-0">
-            <Calendar />
+            <Calendar db_events={db_events} onDeleteClicked={onDelete} />
           </div>
         </div>
       </main>
