536f40e37d61a86ec81725758db7d12885284043
jeongah2651 <46205870+jeongah2651@users.noreply.github.com>
2024-05-17 01:37:02 +0900

    feat: 불필요한 주석 제거

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -153,11 +153,6 @@ const ProfilePage: FC<Props> = ({ userProfile }) => {
       </nav>
       <div className="container mx-auto flex max-w-sm flex-1 flex-col gap-4 pb-[50px] pt-4">
         <div className="relative mx-auto my-5 flex h-80 w-80 items-center justify-center">
-          {/* <button type="button" hidden={!editMode} className="absolute right-2.5 top-0" onClick={() => handleImageButton()}>
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M16.8617 4.48667L18.5492 2.79917C19.2814 2.06694 20.4686 2.06694 21.2008 2.79917C21.9331 3.53141 21.9331 4.71859 21.2008 5.45083L6.83218 19.8195C6.30351 20.3481 5.65144 20.7368 4.93489 20.9502L2.25 21.75L3.04978 19.0651C3.26323 18.3486 3.65185 17.6965 4.18052 17.1678L16.8617 4.48667ZM16.8617 4.48667L19.5 7.12499" stroke="#0F172A" strokeWidth="1.5" strokeLinejoin="round"/>
-                            </svg>
-                        </button> */}
           <ProfileIcon imageUrl={imageUrl} width="100%" height="100%" fill="#CCCFC4" />
           {editMode && (
             <label
